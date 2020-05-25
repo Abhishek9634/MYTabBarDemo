27329fa03c4c38bacf46d34318c842d635ee5d8c
Abhishek <abhishekthapliyal7579@gmail.com>
2020-05-25 20:57:13 +0530

    #1

--- MYTabBarDemo/ViewControllers/Tab1ViewController.swift
+++ MYTabBarDemo/ViewControllers/Tab1ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class Tab1ViewController: UIViewController {
-
+    
     static var newInstance: Tab1ViewController {
         let storyboard = UIStoryboard(name: "tab1", bundle: nil)
         let vc = storyboard.instantiateViewController(
@@ -20,19 +20,6 @@ class Tab1ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
