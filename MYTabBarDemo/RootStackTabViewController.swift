6101ff84bb0706407abaebe661996c82688a8a47
Abhishek <abhishekthapliyal7579@gmail.com>
2020-05-31 09:12:25 +0530

    #12

--- MYTabBarDemo/RootStackTabViewController.swift
+++ MYTabBarDemo/RootStackTabViewController.swift
@@ -25,7 +25,7 @@ class RootStackTabViewController: UIViewController {
     lazy var tabModels: [BottomStackItem] = {
         return [
             BottomStackItem(title: "Home", image: "home"),
-            BottomStackItem(title: "Likes", image: "heart"),
+            BottomStackItem(title: "Favorites", image: "heart"),
             BottomStackItem(title: "Search", image: "search"),
             BottomStackItem(title: "Profile", image: "user"),
             BottomStackItem(title: "Settings", image: "settings")
